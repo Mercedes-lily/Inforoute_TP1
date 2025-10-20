3942952d13bc2801a94ee92b6565df501cf5b550
vboulang <boulangerv8@gmail.com>
2025-10-20 04:51:18 -0400

    setting database

--- TP1/Recuperation_donnees_CKAN/Recuperation_donnees_CKAN/settings.py
+++ TP1/Recuperation_donnees_CKAN/Recuperation_donnees_CKAN/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Recuperation'
+    'Recuperation',
+    'drf_yasg',
+    'rest_framework',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
