1b64e9e509dd60694fcd7827f0b8ecc44e83d8b6
vboulang <boulangerv8@gmail.com>
2025-10-21 22:01:18 -0400

    Create serializers.py

---
+++ Recuperation_donnees_CKAN/Recuperation/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Regroupement, Etablissement, IDE, IMSE, Coordonnee, SFR
+
+class CoordonneeSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Coordonnee
+		fields =("adresse", "municipalite", "code_postal", "site", "telephone")
+
+class RegroupementSerializer(serializers.ModelSerializer):
+	coordonnee = CoordonneeSerializer(read_only=True) #lors de l<ajouy de regroupement, on ne peut ajouter de coordonnee
+	class Meta:
+		model = Regroupement
+		fields = ("code", "nom", "nom_court", "coordonnee", "superficie", "perimetre", "langue")
+
+class SFRSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = SFR
+		fields = ("indice", "rang")
+
+class IMSESerializer(serializers.ModelSerializer):
+	class Meta:
+		model = IMSE
+		fields = ("indice", "rang")
+
+class IDESerializer(serializers.ModelSerializer):
+	imse = IMSESerializer(read_only=True)
+	sfr = SFRSerializer(read_only=True)
+	class Meta:
+		model = IDE
+		fields = ("sfr", "imse", "defavorisation")
+
+class EtablissementSerializer(serializers.ModelSerializer):
+	coordonnee = CoordonneeSerializer(read_only=True)
+	ide = IDESerializer(read_only=True)
+	regroupement = RegroupementSerializer(read_only=True)
+	class Meta:
+		model = Regroupement
+		fields = ("codeOrg", "CodeImm", "nom", "coordonnee", "prescolaire", "primaire", "secondaire", "professionnel", "adulte", "type", "ide", "regroupement")
