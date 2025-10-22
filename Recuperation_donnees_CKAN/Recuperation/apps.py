ddaa43e6583ef1ff023ff1a1b210ba626f31ceda
vboulang <boulangerv8@gmail.com>
2025-10-22 16:28:33 -0400

    Nettoyage : Arrêt du suivi des fichiers générés par Python (pycache, .pyc)

--- Recuperation_donnees_CKAN/Recuperation/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecuperationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Recuperation'
--- Recuperation_donnees_CKAN/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import EtablissementAPIView
-
-urlpatterns = [
-    path('ecole/', EtablissementAPIView.as_view(), name = 'ecole_api'),
-]
