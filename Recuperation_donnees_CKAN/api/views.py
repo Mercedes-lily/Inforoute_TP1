5e319dd60a645810cd6d8104e3d48c7773a17f8e
Valerie <stpierre.v2016@gmail.com>
2025-10-25 10:24:40 -0400

    path fix

--- Recuperation_donnees_CKAN/api/views.py
+++ Recuperation_donnees_CKAN/api/views.py
@@ -1,13 +1,22 @@
 from rest_framework.views import APIView, status, Response
 from Recuperation.models import Etablissement
 from .serializers import EtablissementSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 
 class EtablissementAPIView(APIView):
+    @swagger_auto_schema(tags=['Etablissements'],
+    operation_description="Récupérer toutes les écoles",
+    responses= {200 : EtablissementSerializer(many=True)})
     def get(self, request):
         etablissements = Etablissement.objects.all()
         etablissements_serializer = EtablissementSerializer(etablissements, many=True)
         return Response(etablissements_serializer.data, status = status.HTTP_200_OK)
     
+    @swagger_auto_schema(tags=['Etablissements'],
+    operation_description="Ajouter une école",
+    responses= {201 : "Établissement créé", 400:"Bad Request"})
     def post(self, request):
         etablissements_serializer = EtablissementSerializer(data=request.data)
         if etablissements_serializer.is_valid():
@@ -15,7 +24,23 @@ class EtablissementAPIView(APIView):
             return Response({"message" : "Valide"}, status = status.HTTP_201_CREATED)
         else:
            return Response(etablissements_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-         
+    
+    # @swagger_auto_schema(
+    #     tags=['Etablissements'],
+    #     operation_description="Supprimé une école par son ID",
+    #     request_body=openapi.Schema(
+    #         type=openapi.TYPE_OBJECT,
+    #         properties={
+    #             "id": openapi.Schema(
+    #                 type=openapi.TYPE_INTEGER,
+    #                 description="ID de l'école à supprimer",
+    #             )
+    #         },
+    #         required=["id"], 
+    #     ),
+    #     responses= {204 : "Établissement supprimé", 404:"Not Found"}
+    # )
+
     def delete(self, request, etablissement_id):
         try:
             etablissement = Etablissement.objects.get(id=etablissement_id)
